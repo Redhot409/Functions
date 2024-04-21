c287236c9ddddbe3454f0868b2a106c5e3e4abd7
Alexander Melikhov <alexandersermel@gmail.com>
2024-04-21 23:04:53 +0300

    Templated functions except FillRand

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,34 +6,26 @@ void FillRand(int Arr[],const long N, int minRand=0, int maxRand=100);
 void FillRand(double Arr[],const long N, int minRand=0, int maxRand=100);
 void FillRand(char Arr[],const long N, int minRand=0, int maxRand=100);
 
-void Print(const int Arr[], const long N);
-void Print(const double Arr[], const long N);
-void Print(const char Arr[], const long N);
+template<typename T>
+void Print(const T Arr[], const long N);
 
-int Sum(const int Arr[],const long N);
-double Sum(const double Arr[],const long N);
-char Sum(const char Arr[],const long N);
+template<typename T>
+T Sum(const T Arr[],const long N);
 
-double Avg_value(const int Arr[], long N);
-double Avg_value(const double Arr[], long N);
-char Avg_value(const char Arr[], long N);
+template<typename T>
+double Avg_value(const T Arr[], long N);
 
-int Min_value(const int Arr[], long N);
-double Min_value(const double Arr[], long N);
-char Min_value(const char Arr[], long N);
+template<typename T>
+T Min_value(const T Arr[], long N);
 
-int Max_value(const int Arr[], long N);
-double Max_value(const double Arr[], long N);
-char Max_value(const char Arr[], long N);
+template<typename T>
+T Max_value(const T Arr[], long N);
 
-void Shift_left( int Arr[], long N,int number_of_shift);
-void Shift_left( double Arr[], long N, int number_of_shift);
-void Shift_left( char Arr[], long N, int number_of_shift);
+template<typename T>
+void Shift_left( T Arr[], long N,int number_of_shift);
 
-void Shift_right(int Arr[], int N, int number_of_shift);
-void Shift_right(double Arr[], int N, int number_of_shift);
-void Shift_right(char Arr[], int N, int number_of_shift);
-
+template<typename T>
+void Shift_right(T Arr[], int N, int number_of_shift);
 
 void main()
 {
@@ -78,22 +70,10 @@ void main()
 	cout << "Максимальное значение элементов массива char :" << Max_value(Crr, nN) << endl;
 	Shift_left(Crr, nN, number_of_shift);
 	Shift_right(Crr, nN, number_of_shift);
+}
 
-}
-void FillRand(int Arr[],const long N,int minRand,int maxRand) 
-{
-	if (maxRand < minRand)
-	{
-		int buffer = minRand;
-		minRand = maxRand;
-		maxRand = buffer;
-	}
-	for (int i = 0; i < N; i++)
-	{
-		Arr[i] = minRand + rand() % (maxRand - minRand);
-	}
-}
-void Print(const int Arr[], long N)
+template<typename T>
+void Print(const T Arr[], long N)
 {
 	cout << "Элементы массива случайных чисел :";
 	for (int i = 0; i < N; i++)
@@ -102,43 +82,48 @@ void Print(const int Arr[], long N)
 	}
 	cout << endl;
 }
-int Sum(const int Arr[], long N)
+template<typename T>
+T Sum(const T Arr[], long N)
 {
-	int sum = 0;
+	T sum = 0;
 	for (int i = 0; i < N - 1; i++)
 	{
 		sum += Arr[i];
 	}
 	return sum;
 }
-int Min_value(const int Arr[], long N)
+template<typename T>
+T Min_value(const T Arr[], long N)
 {
-	int min = 0;
+	T min = 0;
 	for (int i = 0; i < N; i++)
 	{
 		if (Arr[i] < min) min = Arr[i];
 	}	return min;
 }
-double Avg_value(const int Arr[], const long N)
+template<typename T>
+double Avg_value(const T Arr[], const long N)
 {
 	{
 		return Sum(Arr, N) / (double)N;
 	}
 }
-int Max_value(const int Arr[], long N)
+template<typename T>
+T Max_value(const T Arr[], long N)
 {
-	int max = 0;
+	T max = 0;
 	for (int i = 0; i < N; i++)
 	{
 		if (Arr[i] > max) max = Arr[i];
 	}	return max;
 }
-void Shift_left(int Arr[], long N, int number_of_shift)
+template<typename T>
+void Shift_left(T Arr[], long N, int number_of_shift)
 {
 	cout << "Массив сдвинутых произвольных элементов влево  :";
 	for (int i = 0; i < (number_of_shift) % N; i++)
 	{
-		int buff = Arr[0];
+		T buff = Arr[0];
 		for (int i = 0; i < N - 1; i++)
 		{
 			Arr[i] = Arr[i + 1];
@@ -151,12 +136,13 @@ void Shift_left(int Arr[], long N, int number_of_shift)
 	}
 		cout << endl;
 }
-void Shift_right(int Arr[], int N, int number_of_shift)
+template<typename T>
+void Shift_right(T Arr[], int N, int number_of_shift)
 {
 	cout << "Массив сдвинутых произвольных элементов вправо  :";
 	for (int i = 0; i < (number_of_shift) % N; i++)
 	{
-	    int buff = Arr[N-1];
+	    T buff = Arr[N-1];
 		for (int i = N - 2; i>= 0; i--)
 		{
 			Arr[i + 1] = Arr[i];
@@ -170,6 +156,19 @@ void Shift_right(int Arr[], int N, int number_of_shift)
 		cout << endl;
 }
 
+void FillRand(int Arr[], const long N, int minRand, int maxRand)
+{
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		Arr[i] = minRand + rand() % (maxRand - minRand);
+	}
+}
 void FillRand(double Arr[], const long N, int minRand, int maxRand) 
 {
 	if (maxRand < minRand)
@@ -186,84 +185,6 @@ void FillRand(double Arr[], const long N, int minRand, int maxRand)
 		Arr[i] /= 100;
 	}
 }
-void Print(const double Arr[], long N)
-{
-	cout << "Вывод элементов массива типа double : ";
-	for (int i = 0; i < N; i++)
-	{
-		cout << Arr[i] << "\t";
-	}
-	cout << endl;
-}
-double Sum(const double Arr[], long N)
-{
-	double sum = 0;
-	for (int i = 0; i < N - 1; i++)
-	{
-		sum += Arr[i];
-	}
-	return sum;
-}
-double Min_value(const double Arr[], long N)
-{
-	double min = 0;
-	for (int i = 0; i < N; i++)
-	{
-		if (Arr[i] < min) min = Arr[i];
-	}	return min;
-}
-double Avg_value(const double Arr[], const long N)
-{
-	{
-		return Sum(Arr, N) / (double)N;
-	}
-}
-double Max_value(const double Arr[], long N)
-{
-	double max = 0;
-	for (int i = 0; i < N; i++)
-	{
-		if (Arr[i] > max) max = Arr[i];
-	}	return max;
-}
-void Shift_left(double Arr[], long N, int number_of_shift)
-{
-	cout << "Массив сдвинутых double элементов влево  :";
-	for(int i = 0; i < (number_of_shift) % N; i++)
-	{ 
-		double buff = Arr[0];
-		for (int i = 0; i < N; i++)
-		{
-		Arr[i] = Arr[i + 1];
-		}
-		Arr[N - 1] = buff;
-	
-	}
-	for (int i = 0; i < N; i++)
-	{
-		cout << Arr[i] << "\t";
-	}
-		cout << endl;
-}
-void Shift_right(double Arr[], int N, int number_of_shift)
-{
-	cout << "Массив сдвинутых double элементов вправо  :";
-	for (int i = 0; i < (number_of_shift) % N; i++)
-	{
-		double buff = Arr[N - 1];
-		for (int i = N - 2; i >= 0; i--)
-		{
-			Arr[i + 1] = Arr[i];
-		}
-		Arr[0] = buff;
-	}
-	for (int i = 0; i < N; i++)
-	{
-		cout << Arr[i] << "\t";
-	}
-	cout << endl;
-}
-
 void FillRand(char Arr[], const long N, int minRand, int maxRand)
 {
 	if (maxRand < minRand)
@@ -279,80 +200,11 @@ void FillRand(char Arr[], const long N, int minRand, int maxRand)
 		Arr[i] =minRand + rand() % (maxRand - minRand);
 	}
 }
-void Print(const char Arr[], long N)
-{
-	cout << "Элементы массива случайных символов :";
-	for (int i = 0; i < N; i++)
-	{
-		cout << Arr[i] << "\t";
-	}
-	cout << endl;
-}
-char Sum(const char Arr[], long N)
-{
-	int sum = 0;
-	for (int i = 0; i < N - 1; i++)
-	{
-		sum += Arr[i];
-	}
-	return  sum;
-}
-char Avg_value(const char Arr[], const long N)
-{
-	{
-		return Sum(Arr, N) / (double)N;
-	}
-}
-char Min_value(const char Arr[], long N)
-{
-	int min = 0;
-	for (int i = 0; i < N; i++)
-	{
-		if (Arr[i] < min) min = Arr[i];
-	}	return min;
-}
-char Max_value(const char Arr[], long N)
-{
-	int max = 0;
-	for (int i = 0; i < N; i++)
-	{
-		if (Arr[i] > max) max = Arr[i];
-	}	return max;
-}
-void Shift_left(char Arr[], long N, int number_of_shift)
-{
-	cout << "Массив сдвинутых char элементов влево  :";
-	for (int i = 0; i < (number_of_shift) % N; i++)
-	{
-		char buff = Arr[0];
-		for (int i = 0; i < N - 1; i++)
-		{
-			Arr[i] = Arr[i + 1];
-		}
-		Arr[N - 1] = buff;
-	}
-	for (int i = 0; i < N; i++)
-	{
-		cout << Arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Shift_right(char Arr[], int N, int number_of_shift)
-{
-	cout << "Массив сдвинутых char элементов вправо  :";
-	for (int i = 0; i < (number_of_shift) % N; i++)
-	{
-		char buff = Arr[N - 1];
-		for (int i = N - 2; i >= 0; i--)
-		{
-			Arr[i + 1] = Arr[i];
-		}
-		Arr[0] = buff;
-	}
-	for (int i = 0; i < N; i++)
-	{
-		cout << Arr[i] << "\t";
-	}
-	cout << endl;
-}
+
+
+
+
+
+
+
 
